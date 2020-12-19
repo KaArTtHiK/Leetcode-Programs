bd6d1c3306c4415e0bad8988cd867b7c04dc32c0
KaArTtHiK <sirigirikarthik00@gmail.com>
2020-12-19 22:18:34 +0530

    median of integers

---
+++ Median.py
@@ -0,0 +1,20 @@
+import statistics
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        for i in nums2:
+            nums1.append(i)
+        nums1.sort()
+        return (statistics.median(nums1))
+		
+"""
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+"""
